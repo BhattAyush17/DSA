126532e73dbff75da04e597b91cbcdb625c4d9cc
BhattAyush17 <ayush04bhatt@gmail.com>
2025-10-12 18:34:15 +0530

    linked list rep of trees

--- DSA/ComplexDS/LinkedListRepresenation03.cpp
+++ DSA/ComplexDS/LinkedListRepresenation03.cpp
@@ -1,1 +1,35 @@
 // Linked List Representation
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+void inorder(Node* root) {
+    if (root == NULL) return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+int main() {
+    // Creating nodes
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+    cout << "Inorder Traversal: ";
+    inorder(root);
+
+    return 0;
+}
