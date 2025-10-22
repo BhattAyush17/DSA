6ff625291b7b231a1ef4c8a9308a07560b3a24e8
BhattAyush17 <ayush04bhatt@gmail.com>
2025-10-22 15:40:49 +0530

    Update Trees01.cpp

--- DSA/ComplexDS/Trees01.cpp
+++ DSA/ComplexDS/Trees01.cpp
@@ -37,15 +37,20 @@ int main() {
     //      2   3
     //     / \
     //    4   5
+    //       / \
+    //      6   7
     Node* root = new Node(1);
     root->left = new Node(2);
     root->right = new Node(3);
     root->left->left = new Node(4);
     root->left->right = new Node(5);
+    root->left->right->left = new Node(6);
+    root->left->right->right = new Node(7);
 
     cout << "Inorder Traversal of Tree: ";
     inorderTraversal(root);
     cout << endl;
+
 
     return 0;
 }
