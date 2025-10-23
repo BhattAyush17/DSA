ea5610c6b442d6f557c5fa3fda1a78c9ca828f92
BhattAyush17 <ayush04bhatt@gmail.com>
2025-10-23 17:59:21 +0530

    added an additional node to the prev tree

--- DSA/ComplexDS/Trees01.cpp
+++ DSA/ComplexDS/Trees01.cpp
@@ -39,6 +39,8 @@ int main() {
     //    4   5
     //       / \
     //      6   7
+    //     /
+    //    8
     Node* root = new Node(1);
     root->left = new Node(2);
     root->right = new Node(3);
@@ -46,6 +48,7 @@ int main() {
     root->left->right = new Node(5);
     root->left->right->left = new Node(6);
     root->left->right->right = new Node(7);
+    root->left->right->right->left = new Node(8);
 
     cout << "Inorder Traversal of Tree: ";
     inorderTraversal(root);
